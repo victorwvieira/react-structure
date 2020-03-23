d3d0063ac1633e94d74fd6109a964e1bde6861c0
Victor <v.w.vieira@accenture.com>
2020-03-23 12:56:01 +0000

    Added node to the project and developed server to run using Heroku.

---
+++ build/precache-manifest.c5ede5230a171b51b050b37456d098e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3987705ab1a9f58a561faa253aa33bfc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "acd6d93653a265052841",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "659d2792ae4388fb83fb",
+    "url": "/static/js/2.ea56aced.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/static/js/2.ea56aced.chunk.js.LICENSE"
+  },
+  {
+    "revision": "acd6d93653a265052841",
+    "url": "/static/js/main.1e9faa99.chunk.js"
+  },
+  {
+    "revision": "24ebb1c3dd08df1c2b73",
+    "url": "/static/js/runtime-main.52f759f6.js"
+  }
+]);
